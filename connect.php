912f7151f6cd9979e767ce6a3365d74385712b6c
nguyentienbinh2408 <61313206+nguyentienbinh2408@users.noreply.github.com>
2020-05-25 10:26:00 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 	
-    $connection = mysqli_connect("localhost:3325", "root", "") or die("Khong the ket noi den database");
+    $connection = mysqli_connect("localhost", "root", "") or die("Khong the ket noi den database");
     mysqli_select_db($connection, "cinema") or die("Khong tim thay database");
   	mysqli_set_charset($connection, 'UTF8');
 	
